bc3c6f47982202661c1fb0a5c25cb505002d3fb8
Tripti Sharma <70108118+Tripti1611@users.noreply.github.com>
2022-07-07 00:39:51 +0530

    Add files via upload

---
+++ Client/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+
+# Create your views here.
+def email(request):
+	return render(request,"Client/email.html")
+
+def mail_info(request):
+	mailto = request.POST['to']
+	mailsubject = request.POST['subject']
+	mailmessage = request.POST['msg']
+
+	send_mail(mailsubject, mailmessage, settings.EMAIL_HOST_USER, [mailto], fail_silently=False)
+	messages.success(request, 'Email Sent Successfully!')
+	return redirect('/Client/email/')
